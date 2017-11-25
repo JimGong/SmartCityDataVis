1b6669d6ca42f635b3636597384b5879307eea53
JiaMin Gong <JiaMinGong@a.local>
2017-11-25 04:33:47 -0500

    Update
    This version will animate line. The speed of drawing the line is
    constant.

--- JS/utility.js
+++ JS/utility.js
@@ -11,6 +11,7 @@ function getLat(roadId, stationId, stationData){
 		}
 	}
 }
+
 function getLon(roadId, stationId, stationData){
 	for(var i=0; i<stationData.length; i++){
 		var curRow=stationData[i];
@@ -18,4 +19,21 @@ function getLon(roadId, stationId, stationData){
 			return curRow["Lon"];
 		}
 	}
+}
+
+function getY(x, x1,y1,x2,y2){
+	var slop =  (y2-y1)/(x2-x1);
+	var y= slop*(x-x1)+y1;
+	return y;
+}
+
+function drawDots(sp, ep, map, geojson, speedFactor,interval){
+	for(var i=0;i<speedFactor;i++){
+		var x=sp[0]+ i*interval;
+		var y= getY(x,sp[0], sp[1],ep[0],ep[1]);
+		console.log(x,y);
+		geojson.features[0].geometry.coordinates.push([x,y]);
+		map.getSource('route').setData(geojson);
+	}
+
 }
